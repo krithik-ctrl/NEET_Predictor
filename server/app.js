cc4056d9465ae42d1ba23861da55a3637e3beb2c
krithik-ctrl <krithik@webscafe.in>
2026-06-23 14:52:31 +0530

    feat: complete backend infrastructure setup

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import routes from "./routes/index.js";
+
+import notFound from "./common/middlewares/notFound.js";
+import errorHandler from "./common/middlewares/errorHandler.js";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", routes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
