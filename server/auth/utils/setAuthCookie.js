ab34b291de8969f11737f7ed79e96d1f36982974
krithik-ctrl <krithik@webscafe.in>
2026-07-10 21:55:56 +0530

    Refactor setAuthCookie to adjust cookie options

--- server/auth/utils/setAuthCookie.js
+++ server/auth/utils/setAuthCookie.js
@@ -1,19 +1,8 @@
-
-
-export const setAuthCookie = (
-  res,
-  token
-) => {
+export const setAuthCookie = (res, token) => {
   res.cookie("accessToken", token, {
     httpOnly: true,
-
-    secure:
-      process.env.NODE_ENV ===
-      "production",
-
-    sameSite: "strict",
-
-    maxAge:
-      7 * 24 * 60 * 60 * 1000,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
